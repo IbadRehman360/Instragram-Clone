31a6c143db3328f901bb45044904faf1ab045a91
IbadRehman360 <ibadhashim4@gmail.com>
2024-02-06 14:46:40 +0500

    new changes pushed

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const compression = require("compression");
 const helmet = require("helmet");
+const path = require("path");
 
 const connectDB = require("./config/db.config");
 
@@ -23,21 +24,23 @@ app.use(morgan("dev"));
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://instragram-clone-one.vercel.app/",
         credentials: true,
     })
 );
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
 require("./routes/auth.route")(app);
 require("./routes/post.route")(app);
 require("./routes/user.route")(app);
 
 
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+});
 const PORT = process.env.PORT || 3001;
 
 app.disable("x-powered-by");
